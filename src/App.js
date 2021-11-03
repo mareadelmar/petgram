933ca66ebe940757502d5506fa53414c517effac
mareadelmar <mar.ortega.v@gmail.com>
2021-11-03 17:30:38 -0300

    Route protection fix

--- src/App.js
+++ src/App.js
@@ -9,44 +9,38 @@ import Favs from "./pages/Favs";
 import User from "./pages/User";
 import PublicRoute from "./pages/PublicRoute";
 import AuthContext from "./context/AuthContext";
-
+import PhotosContext from "./context/PhotosContext";
 // const UserLogged = ({ children }) => {
 // 	return children({ isAuth: false });
 // };
 
 function App() {
-	//const { isAuth } = useContext(AuthContext);
+	const { isAuth } = useContext(AuthContext);
+	const { photos } = useContext(PhotosContext);
+
+	console.log(isAuth);
 	return (
 		<div className='App'>
 			<GlobalStyle />
 			<BrowserRouter>
+				{photos.length}
 				<Header />
 				<Switch>
 					<Route path='/detail/:id' component={Detail} />
 					<Route path='/pets/:categoryId' component={Home} />
 					<Route exact path='/' component={Home} />
 
-					<AuthContext.Consumer>
-						{({ isAuth }) =>
-							isAuth ? (
-								<Switch>
-									<Route path='/favs' component={Favs} />
-									<Route path='/user' component={User} />
-								</Switch>
-							) : (
-								<Switch>
-									<Route
-										path='/favs'
-										component={PublicRoute}
-									/>
-									<Route
-										path='/user'
-										component={PublicRoute}
-									/>
-								</Switch>
-							)
-						}
-					</AuthContext.Consumer>
+					{isAuth ? (
+						<Switch>
+							<Route path='/favs' component={Favs} />
+							<Route path='/user' component={User} />
+						</Switch>
+					) : (
+						<Switch>
+							<Route path='/favs' component={PublicRoute} />
+							<Route path='/user' component={PublicRoute} />
+						</Switch>
+					)}
 
 					{/* <Route path='/category/:id' /> */}
 				</Switch>
