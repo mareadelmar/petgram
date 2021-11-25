de32e3a235fc6d824e1a04b0ffe6df9f6d18b784
mareadelmar <mar.ortega.v@gmail.com>
2021-11-25 20:16:44 -0300

    Add sessionStorage to auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,13 +3,16 @@ import { createContext, useState } from "react";
 const AuthContext = createContext({});
 
 export const AuthContextProvider = ({ children }) => {
-	const [isAuth, setIsAuth] = useState(false);
+	const [isAuth, setIsAuth] = useState(() => {
+		return window.sessionStorage.getItem("token");
+	});
 
 	const value = {
 		isAuth,
 		activateAuth: token => {
 			if (token) {
 				setIsAuth(true);
+				window.sessionStorage.setItem("token", token);
 			}
 		},
 	};
