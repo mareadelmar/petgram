0a5f25892550970e71eab84a4ae5768ed916467e
mareadelmar <mar.ortega.v@gmail.com>
2021-11-25 19:42:00 -0300

    Add styles to Login component

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form, Input, Button, Title } from "./styles";
 
 const useInputValue = initialValue => {
 	const [value, setValue] = useState(initialValue);
@@ -7,16 +8,17 @@ const useInputValue = initialValue => {
 	return { value, onChange };
 };
 
-const LoginForm = () => {
+const LoginForm = ({ title }) => {
 	const email = useInputValue("");
 	const password = useInputValue("");
 
 	return (
-		<form>
-			<input type='text' placeholder='Email' {...email} />
-			<input type='password' placeholder='Password' {...password} />
-			<button>iniciar sesión</button>
-		</form>
+		<Form>
+			<Title>{title}</Title>
+			<Input type='text' placeholder='Email' {...email} />
+			<Input type='password' placeholder='Password' {...password} />
+			<Button>{title}</Button>
+		</Form>
 	);
 };
 
---
+++ src/components/LoginForm/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+	padding: 16px;
+`;
+
+export const Input = styled.input`
+	border: 1px solid #ccc;
+	border-radius: 3px;
+	margin-bottom: 8px;
+	padding: 8px;
+	display: block;
+	width: 100%;
+	margin: 16px 0;
+`;
+
+export const Button = styled.button`
+	background-color: #8d00ff;
+	border-radius: 3px;
+	color: #fff;
+	text-align: center;
+	display: block;
+	width: 100%;
+	height: 32px;
+	cursor: pointer;
+	font-size: 16px;
+	font-weight: bolder;
+`;
+
+export const Title = styled.h2`
+	font-size: 16px;
+	font-weight: bolder;
+	padding: 8px 0;
+`;
